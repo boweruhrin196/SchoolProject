4dbff1a118ca661b77216e21005377b280b814d4
boweruhrin196 <boweruhrin196@gmail.com>
2025-03-29 18:53:04 +0000

    Add 21.py

---
+++ src/21.py
@@ -0,0 +1,5 @@
+import os
+os.system("cd SchoolProject && git init")
+os.chdir('SchoolProject')
+os.system('git remote add origin https://github.com/username/SchoolProject.git')
+os.system('git pull origin master')
